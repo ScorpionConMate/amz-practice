853241b7ddfea47bd56b2c67f1331f1f7d1ebe7a
Alan Perez <diegoalanperezz@gmail.com>
2025-07-28 10:54:28 -0300

    Add comments #121

--- 121-best-time-stock.js
+++ 121-best-time-stock.js
@@ -5,15 +5,21 @@ const data = require("./121-input");
  * @returns {number}
  */
 function maxProfit(prices) {
-  let profit = [];
+  let profit = 0;
   for (let index = 0; index < prices.length; index++) {
+    // Valor actual para comprar
     const buyOn = prices[index];
+
+    // Si no tiene un valor mas alto en los dias siguientes, continuamos con el siguiente elemento del array
     const haveGreater = Math.max(...prices.slice(index + 1));
     if (haveGreater <= buyOn) continue;
-    profit.push(haveGreater - buyOn);
+    // Revisamos el profit actual
+    const currentProfit = haveGreater - buyOn;
+    // Si el profit actual, es mayor al que teniamos, actualizamos el profit
+    if (currentProfit > profit) profit = currentProfit
   }
 
-  return profit.length > 0 ? Math.max(...profit) : 0;
+  return profit;
 }
 
 // Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
@@ -23,8 +29,3 @@ console.log(maxProfit([7, 1, 5, 3, 6, 4])); // 5
 // Explanation: In this case, no transactions are done and the max profit = 0.
 console.log(maxProfit([7, 6, 4, 3, 1])); // 0
 console.log(maxProfit(data));
-/* (async () => {
-    const data = await readFile('./121-input.json');
-
-    console.log(maxProfit(data))
-})() */
